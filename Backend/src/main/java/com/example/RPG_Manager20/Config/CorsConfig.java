5305eb684142d127cd053cb3aa6692726884d216
im-a-Cloud <prudencioalvaro10@gmail.com>
2026-07-15 08:45:06 -0300

    Atualização no BD

--- Backend/src/main/java/com/example/RPG_Manager20/Config/CorsConfig.java
+++ Backend/src/main/java/com/example/RPG_Manager20/Config/CorsConfig.java
@@ -9,7 +9,6 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-
         registry.addMapping("/**")
                 .allowedOrigins(getAllowedOrigins())
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
